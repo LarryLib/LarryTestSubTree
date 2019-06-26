43a93be6f932b76569296d719676cdbf533b1301
Larry_iMac <lxd_super_main@126.com>
2019-06-26 15:38:48 +0800

    init project：Add

---
+++ LarryTestSubTree/Func.swift
@@ -0,0 +1,14 @@
+//
+//  Func.swift
+//  LarryTestSubTree
+//
+//  Created by Larry_iMac on 2019/6/26.
+//  Copyright © 2019 Larry_iMac. All rights reserved.
+//
+
+import Foundation
+
+/// 整数求和
+func add(_ a: Int, _ b: Int) -> Int {
+    return a + b
+}
---
+++ LarryTestSubTree/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LarryTestSubTree
+//
+//  Created by Larry_iMac on 2019/6/26.
+//  Copyright © 2019 Larry_iMac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
